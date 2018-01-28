f807cc447921ab8be85854d64458ea5de757e479
davidquero <jose.david.quero.sanchez@gmail.com>
2018-01-28 20:48:49 +0100

    Continuación

--- Pinball/Assets/Scripts/FlipperScript.cs
+++ Pinball/Assets/Scripts/FlipperScript.cs
@@ -14,16 +14,30 @@ public class FlipperScript : MonoBehaviour {
     // Nombre de la entrada
     public string inputName;
 
+    HingeJoint hingeJoint;
+
 	// Use this for initialization
 	void Start () {
-		
+        hingeJoint = GetComponent<HingeJoint>();
+        hingeJoint.useSpring = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        JointSpring spring = new JointSpring();
+        spring.spring = hitStrength;
+        spring.damper = flipperDamper;
+
 		if (Input.GetAxis(inputName) == 1)
         {
-
+            spring.targetPosition = pressedPostion;
         }
+        else
+        {
+            spring.targetPosition = restPosition;
+        }
+
+        hingeJoint.spring = spring;
+        hingeJoint.useLimits = true;
 	}
 }
